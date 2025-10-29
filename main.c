504e8adc0718c8953e19048c9582dfcbfa9b4c49
RShaxinya <shaxathrasher@gmail.com>
2025-10-29 19:00:34 +0500

    main.c

--- main.c
+++ main.c
@@ -5,18 +5,18 @@
 #define LED_BLUE    25
 #define LED_YELLOW  26
 
-// Простая функция задержки
+
 void simple_delay(uint32_t milliseconds) {
     for (volatile uint32_t i = 0; i < milliseconds * 1000; i++) {
-        // Пустой цикл
+        
     }
 }
 
 void blink_single_led(uint32_t led_pin, int count) {
     for (int i = 0; i < count; i++) {
-        nrf_gpio_pin_write(led_pin, 0);  // ВКЛ
+        nrf_gpio_pin_write(led_pin, 0);  
         simple_delay(300);
-        nrf_gpio_pin_write(led_pin, 1);  // ВЫКЛ
+        nrf_gpio_pin_write(led_pin, 1); 
         simple_delay(300);
     }
 }
@@ -34,12 +34,12 @@ int main(void) {
 
     while (true) {
         blink_single_led(LED_RED, 6);
-        simple_delay(1000);  // Пауза 1 сек
+        simple_delay(1000);  
         
         blink_single_led(LED_GREEN, 4);
-        simple_delay(1000);  // Пауза 1 сек
+        simple_delay(1000);  
         
-        simple_delay(2000);  // Пауза 2 сек (вместо синего)
+        simple_delay(2000); 
         
         blink_single_led(LED_YELLOW, 4);
         simple_delay(2000);  // Пауза 2 сек
