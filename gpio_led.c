a1ec042a949d5d418be6eab0315dd692367d971c
RShaxinya <shaxathrasher@gmail.com>
2025-11-10 18:37:09 +0500

    gpio_led.c

--- gpio_led.c
+++ gpio_led.c
@@ -1,23 +1,72 @@
-#include "nrf_gpio.h"
+#include "gpio_led.h"
 
-static int button_counter = 0;
+static int sequence_position = 0;
+static bool was_button_pressed = false;
+static int button_press_time = 0;
+static bool led_paused = false;
+static led_color_t current_led_color = LED_OFF;
 
-void nrf_gpio_cfg_output(uint32_t pin) {
-    
+void gpio_led_init(void) {
+    nrf_gpio_cfg_output(LED_RED);
+    nrf_gpio_cfg_output(LED_GREEN);
+    nrf_gpio_cfg_output(LED_BLUE);
+    nrf_gpio_cfg_input(BUTTON_PIN, NRF_GPIO_PIN_PULLUP);
+    set_led_color(LED_OFF);
 }
 
-void nrf_gpio_cfg_input(uint32_t pin, uint32_t pull) {
+void set_led_color(led_color_t color) {
+    nrf_gpio_pin_write(LED_RED, 1);
+    nrf_gpio_pin_write(LED_GREEN, 1);
+    nrf_gpio_pin_write(LED_BLUE, 1);
     
-}
-
-void nrf_gpio_pin_write(uint32_t pin, uint32_t value) {
+    current_led_color = color;
     
-}
-
-uint32_t nrf_gpio_pin_read(uint32_t pin) {
-    button_counter++;
-    if (button_counter % 1000 == 0) {
-        return 0; 
+    switch (color) {
+        case LED_RED_COLOR: nrf_gpio_pin_write(LED_RED, 0); break;
+        case LED_GREEN_COLOR: nrf_gpio_pin_write(LED_GREEN, 0); break;
+        case LED_BLUE_COLOR: nrf_gpio_pin_write(LED_BLUE, 0); break;
+        case LED_OFF: break;
     }
-    return 1; 
+}
+
+led_color_t char_to_led_color(char c) {
+    switch (c) {
+        case 'R': return LED_RED_COLOR;
+        case 'G': return LED_GREEN_COLOR;
+        case 'B': return LED_BLUE_COLOR;
+        default: return LED_OFF;
+    }
+}
+
+void process_led_sequence(bool button_pressed) {
+    const int led_duration_ms = 500;
+    static int time_counter = 0;
+    
+    if (button_pressed && !was_button_pressed) {
+        button_press_time = time_counter;
+        was_button_pressed = true;
+        led_paused = false;
+    }
+    else if (!button_pressed && was_button_pressed) {
+        was_button_pressed = false;
+        if (current_led_color != LED_OFF) {
+            led_paused = true;
+        }
+    }
+    
+    if (button_pressed) {
+        int press_duration = time_counter - button_press_time;
+        
+        if (!led_paused) {
+            int led_index = (press_duration / led_duration_ms) % SEQUENCE_LEN;
+            led_color_t new_color = char_to_led_color(LED_SEQUENCE[led_index]);
+            set_led_color(new_color);
+            sequence_position = (led_index + 1) % SEQUENCE_LEN;
+        }
+    }
+    else if (!led_paused) {
+        set_led_color(LED_OFF);
+    }
+    
+    time_counter++;
 }
