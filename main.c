747d51c28ec8ad37ff673e2f279a25a137cf34f6
RShaxinya <shaxathrasher@gmail.com>
2025-11-12 19:10:43 +0500

    main.c

--- main.c
+++ main.c
@@ -1,9 +1,90 @@
-#include "nrf_gpio.h"
-#include "gpio_led.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+
+#define BUTTON_PIN ((1 << 5) | 6)   // PORT 1, PIN 6
+#define LED_RED    ((0 << 5) | 8)   // PORT 0, PIN 8
+#define LED_GREEN  ((1 << 5) | 9)   // PORT 1, PIN 9  
+#define LED_BLUE   ((0 << 5) | 12)  // PORT 0, PIN 12
+
+
+void nrf_gpio_cfg_output(uint32_t pin) {
+    
+}
+
+void nrf_gpio_cfg_input(uint32_t pin, uint32_t pull) {
+    
+}
+
+void nrf_gpio_pin_write(uint32_t pin, uint32_t value) {
+    
+}
+
+uint32_t nrf_gpio_pin_read(uint32_t pin) {
+    
+    static int counter = 0;
+    counter++;
+    return (counter % 20 == 0) ? 0 : 1;
+}
+
 
 void simple_delay(uint32_t milliseconds) {
     for (volatile uint32_t i = 0; i < milliseconds * 1000; i++);
 }
+
+
+void set_rgb_color(bool red, bool green, bool blue) {
+    nrf_gpio_pin_write(LED_RED, !red);
+    nrf_gpio_pin_write(LED_GREEN, !green); 
+    nrf_gpio_pin_write(LED_BLUE, !blue);
+}
+
+
+void gpio_led_init(void) {
+    
+    nrf_gpio_cfg_input(BUTTON_PIN, 1); // PULLUP
+    
+    
+    nrf_gpio_cfg_output(LED_RED);
+    nrf_gpio_cfg_output(LED_GREEN);
+    nrf_gpio_cfg_output(LED_BLUE);
+    
+    
+    nrf_gpio_pin_write(LED_RED, 1);
+    nrf_gpio_pin_write(LED_GREEN, 1);
+    nrf_gpio_pin_write(LED_BLUE, 1);
+}
+
+
+void process_led_sequence(bool button_state) {
+    static int color_state = 0;
+    static int counter = 0;
+    
+    if (button_state) {
+        
+        switch(color_state) {
+            case 0: set_rgb_color(1, 0, 0); break; // Красный
+            case 1: set_rgb_color(0, 1, 0); break; // Зеленый
+            case 2: set_rgb_color(0, 0, 1); break; // Синий
+            case 3: set_rgb_color(1, 1, 0); break; // Желтый
+            case 4: set_rgb_color(0, 1, 1); break; // Голубой
+            case 5: set_rgb_color(1, 0, 1); break; // Пурпурный
+        }
+        color_state = (color_state + 1) % 6;
+    } else {
+        
+        switch(counter % 6) {
+            case 0: set_rgb_color(1, 0, 0); break;
+            case 1: set_rgb_color(0, 1, 0); break; 
+            case 2: set_rgb_color(0, 0, 1); break;
+            case 3: set_rgb_color(1, 1, 0); break;
+            case 4: set_rgb_color(0, 1, 1); break;
+            case 5: set_rgb_color(1, 0, 1); break;
+        }
+        counter++;
+    }
+}
+
 
 int main(void) {
     gpio_led_init();
@@ -11,6 +92,6 @@ int main(void) {
     while (1) {
         bool button_state = (nrf_gpio_pin_read(BUTTON_PIN) == 0);
         process_led_sequence(button_state);
-        simple_delay(50);
+        simple_delay(500); // Задержка 500ms вместо 50 для лучшей видимости
     }
 }
