12f9f2fd5ae1b3ca20ba1af5edc716cd1b2043d7
RShaxinya <shaxathrasher@gmail.com>
2025-12-02 14:01:34 +0500

    Delete src directory

--- src/nrf_delay.h
+++
@@ -1,9 +0,0 @@
-#ifndef NRF_DELAY_H
-#define NRF_DELAY_H
-
-#include <stdint.h>
-
-void nrf_delay_ms(uint32_t ms);
-void nrf_delay_us(uint32_t us);
-
-#endif
--- src/nrfx_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef NRFX_CONFIG_H
-#define NRFX_CONFIG_H
-
-#include "app_config.h"
-
-#endif
